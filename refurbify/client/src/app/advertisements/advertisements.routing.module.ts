10d247ee78a0afa8ea49474441d769f31a2dba83
Andrea Coluzzi <coluzziandrea.dev@gmail.com>
2021-12-20 21:26:55 +0100

    aggiunta route per dettaglio ad

--- refurbify/client/src/app/advertisements/advertisements.routing.module.ts
+++ refurbify/client/src/app/advertisements/advertisements.routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreateComponent } from './create/create.component';
 import { SearchComponent } from './search/search.component';
+import { ShowAdvertisementComponent } from './show-advertisement/show-advertisement.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: 'search',
     component: SearchComponent,
+  },
+  {
+    path: ':id',
+    component: ShowAdvertisementComponent,
   },
 ];
 
